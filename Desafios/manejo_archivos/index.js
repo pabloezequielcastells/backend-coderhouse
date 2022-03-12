45aa1ede335114f6a692d5b9bdb63f1feea6ca3b
Pablo Castells <pablo.ezequiel.castells@gmail.com>
2022-03-12 18:16:19 -0300

    desafio manejo archivos

--- Desafios/manejo_archivos/index.js
+++ Desafios/manejo_archivos/index.js
@@ -1,4 +1,48 @@
-import { Archivo } from './archivo.js';
+import fs from 'fs';
+class Archivo {
+
+    constructor(nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+        this.productos = null;
+    }
+
+    async leer() {
+        try {
+            if (fs.existsSync(this.nombreArchivo)) {
+                const texto = await fs.promises.readFile(this.nombreArchivo, "utf-8");
+                this.productos = await JSON.parse(texto);
+            } else {
+                this.productos = [];
+            }
+
+            console.log(this.productos);
+            return this.productos;
+        } catch (error) {
+            console.log("No se pudo leer: " + error);
+        }
+    }
+
+    async guardar(producto) {
+        try {
+            if (!this.productos) await this.leer();
+            producto.id = this.productos.length + 1;
+            this.productos.push(producto);
+            await fs.promises.writeFile(this.nombreArchivo, JSON.stringify(this.productos, null, "\t"));
+        } catch (error) {
+            console.log("Error al guardar " + error);
+        }
+    }
+
+    async borrar() {
+        try {
+            await fs.promises.unlink(this.nombreArchivo);
+            console.log("Archivo borrado exitosamente");
+        } catch (error) {
+            console.log("Error al borrar " + error);
+        }
+    }
+
+}
 
 const escuadra = {
     title: "Escuadra",
